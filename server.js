2978fd942afd012292ccd3f2828e4b9476909fce
Aisha Kimpson <akimp313@gmail.com>
2022-04-23 23:03:29 -0400

    added GET test route; server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+// Add the Port designation and the app expression
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// GET Test Route
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+      });
+    });
+
+    // Default response for any other request (Not Found); user requests not supported by the app
+    app.use((req, res) => {
+        res.status(404).end();
+    });
+
+// Add function that starts the express.js serveron the port.
+app.listen (PORT, () => {
+    console.log( 'Server running on port ${PORT}');
+});
