c40adbc3a6e80699aedc515ba768a1ce3f00ee5d
Aisha Kimpson <akimp313@gmail.com>
2022-04-24 13:43:13 -0400

    added db.query to server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 // Add the Port designation and the app expression
@@ -8,14 +10,32 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// Connect the app to the MYSQL database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '@$$2312oK',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 // GET Test Route
+
+// Return all the data in the candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+   });
 
     // Default response for any other request (Not Found); user requests not supported by the app
     app.use((req, res) => {
         res.status(404).end();
     });
 
-// Add function that starts the express.js serveron the port.
+// Add function that starts the express.js server on the port.
 app.listen (PORT, () => {
     console.log( 'Server running on port ${PORT}');
 });
